4607bfe02c9f5a7ab48613d548292dc0eaebf9ef
codeCampWithSifat <sayedhossainsifat300@.com>
2023-03-31 22:32:17 +0600

    full done

--- src/Pages/Dashboard/Dashboard/Payment/CheckoutForm.js
+++ src/Pages/Dashboard/Dashboard/Payment/CheckoutForm.js
@@ -1,11 +1,34 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-const CheckoutForm = ({booking}) => {
+const CheckoutForm = ({ booking }) => {
   const [cardError, setCardError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [transactionId, setTransactionId] = useState("");
+  const [processing, setProcessing] = useState(false);
+
+  // backend intregration code
+  const [clientSecrete, setClientSecret] = useState("");
+  const { price, email, patientName , _id} = booking;
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("http://localhost:5000/create-payment-intent", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `bearer ${localStorage.getItem("accessToken")}`,
+      },
+      body: JSON.stringify({ price }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setClientSecret(data.clientSecret);
+      });
+  }, [price]);
+
   const stripe = useStripe();
   const elements = useElements();
-  const {price} = booking ;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -28,8 +51,55 @@ const CheckoutForm = ({booking}) => {
       console.log(error);
       setCardError(error.message);
     } else {
-        setCardError("")
+      setCardError("");
     }
+
+    setSuccess("");
+    setProcessing(true);
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecrete, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name: patientName,
+            email: email,
+          },
+        },
+      });
+
+    if (confirmError) {
+      setCardError(confirmError.message);
+      return;
+    }
+    if (paymentIntent.status === "succeeded") {
+      //   setSuccess("Congrates Your Payment Successessfully Paid");
+      //   setTransactionId(paymentIntent.id);
+      const payment = {
+        patientName,
+        email,
+        price,
+        transactionId: paymentIntent.id,
+        bookingId  : _id
+      };
+      fetch(`http://localhost:5000/payments`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `bearer ${localStorage.getItem("accessToken")}`,
+        },
+        body: JSON.stringify(payment),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.insertedId) {
+            setSuccess("Congrates Your Payment Successessfully Paid");
+            setTransactionId(paymentIntent.id);
+          }
+        });
+      console.log("paymentIntent", paymentIntent);
+    }
+    setProcessing(false);
   };
   return (
     <div>
@@ -52,11 +122,23 @@ const CheckoutForm = ({booking}) => {
             }}
           />
           <p className="text-red-800 mt-4">{cardError}</p>
-          <button className="btn btn-sm mt-6" type="submit" disabled={!stripe}>
+          <button
+            className="btn btn-sm mt-6"
+            type="submit"
+            disabled={!stripe || !clientSecrete || processing}
+          >
             Pay
           </button>
         </form>
       </div>
+      {success && (
+        <div>
+          <p className="text-violet-800">{success}</p>
+          <p className="text-violet-800 font-bold">
+            Your TrnasactionId : {transactionId}
+          </p>
+        </div>
+      )}
     </div>
   );
 };
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -63,7 +63,7 @@ const router = createBrowserRouter([
             },
             {
                 path : "/dashboard/payment/:id",
-                element : <AdminRoute><Payment/></AdminRoute>,
+                element : <Payment/>,
                 loader : ({params}) => fetch(`http://localhost:5000/bookings/${params.id}`)
             }
         ]
